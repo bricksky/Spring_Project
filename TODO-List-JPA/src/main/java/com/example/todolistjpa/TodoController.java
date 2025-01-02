8724037f19728d57f449f24c39188024f2017a56
bricksky <skydh1214@naver.com>
2025-01-02 18:57:38 +0900

    CRUD 기능 구현

--- TODO-List-JPA/src/main/java/com/example/todolistjpa/TodoController.java
+++ TODO-List-JPA/src/main/java/com/example/todolistjpa/TodoController.java
@@ -1,4 +1,32 @@
 package com.example.todolistjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
 public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @PostMapping("/add")
+    public Todo addTodo(@RequestParam String task){
+        Todo todo = new Todo();
+        todo.setDate(LocalDate.EPOCH);
+        todo.setTask(task);
+        todo.setCompleted(false);
+        todoRepository.save(todo);
+        return todo;
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos(){
+        return todoRepository.findAll();
+    }
 }
--- TODO-List-JPA/src/main/java/com/example/todolistjpa/TodoRepository.java
+++ TODO-List-JPA/src/main/java/com/example/todolistjpa/TodoRepository.java
@@ -1,4 +1,7 @@
 package com.example.todolistjpa;
 
-public class TodoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+                                         // JpaRepository<엔티티타입, @Id타입>
 }
