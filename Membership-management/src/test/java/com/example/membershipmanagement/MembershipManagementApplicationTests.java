f1ddd43899414ef802f38c34727bef11d161441c
bricksky <skydh1214@naver.com>
2025-01-06 17:55:42 +0900

    프로젝트 세팅

---
+++ Membership-management/src/test/java/com/example/membershipmanagement/MembershipManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membershipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
