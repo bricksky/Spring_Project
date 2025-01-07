ae95a171e936f40331772846f8c1b50c65d40f51
bricksky <skydh1214@naver.com>
2025-01-07 16:16:26 +0900

    회원 도메인과 리포지토리 만들기

--- Membership-management/src/main/java/com/example/membershipmanagement/repository/MemoryMemberRepository.java
+++ Membership-management/src/main/java/com/example/membershipmanagement/repository/MemoryMemberRepository.java
@@ -2,10 +2,7 @@ package com.example.membershipmanagement.repository;
 
 import com.example.membershipmanagement.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
 
@@ -22,16 +19,18 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable( store.get(id));
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        return store.values().stream()
+                .filter(m -> m.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return List.of();
+        return new ArrayList<>(store.values());
     }
 }
