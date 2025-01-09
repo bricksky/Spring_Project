70983a8378f7b6877b8ba98356b0c0bf4b5c8e43
bricksky <skydh1214@naver.com>
2025-01-09 15:04:09 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- Membership-management/src/main/java/com/example/membershipmanagement/service/MemberService.java
+++ Membership-management/src/main/java/com/example/membershipmanagement/service/MemberService.java
@@ -3,14 +3,18 @@ package com.example.membershipmanagement.service;
 import com.example.membershipmanagement.domain.Member;
 import com.example.membershipmanagement.repository.MemberRepository;
 import com.example.membershipmanagement.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
