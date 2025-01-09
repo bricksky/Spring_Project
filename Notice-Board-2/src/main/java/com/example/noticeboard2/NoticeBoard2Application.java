fde7f2f82b6297a54ababb790b6f98664f2b229d
bricksky <skydh1214@naver.com>
2025-01-09 20:42:48 +0900

    시작 - 재도전

---
+++ Notice-Board-2/src/main/java/com/example/noticeboard2/NoticeBoard2Application.java
@@ -0,0 +1,13 @@
+package com.example.noticeboard2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoard2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeBoard2Application.class, args);
+    }
+
+}
---
+++ Notice-Board-2/src/test/java/com/example/noticeboard2/NoticeBoard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noticeboard2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoard2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
